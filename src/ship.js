8f0f8bf7b907b59dcac2f5cfc75cc298f6892881
onifernando1 <onifernando1@gmail.com>
2022-11-27 15:35:39 +0000

    ship sunk and hit functions

--- src/ship.js
+++ src/ship.js
@@ -1,8 +1,24 @@
 const Ship = function (length) {
   const shipLength = length;
-  const numberOfHits = null;
-  const sunk = false;
-  return { shipLength, numberOfHits, sunk };
+  let numberOfHits = 0;
+  let sunk = false;
+
+  const hit = function () {
+    numberOfHits += 1;
+    return numberOfHits;
+  };
+
+  const isSunk = function (length, numberOfHits) {
+    if (length == numberOfHits) {
+      sunk = true;
+      return sunk;
+    } else {
+      sunk = false;
+      return sunk;
+    }
+  };
+
+  return { shipLength, numberOfHits, isSunk, hit };
 };
 
 export { Ship };
--- src/ship.test.js
+++ src/ship.test.js
@@ -3,8 +3,20 @@ import { Ship } from "./ship";
 describe("Ship", () => {
   const ship = new Ship();
 
-  test("defines setRule()", () => {
-    expect(ship.sunk).toBe(false);
+  test("Hit increases ship hits by one", () => {
+    expect(ship.hit()).toBe(1);
+  });
+
+  test("Hit increases ship hits by two", () => {
+    expect(ship.hit()).toBe(2);
+  });
+
+  test("Ship sinks when hits = length", () => {
+    expect(ship.isSunk(3, 3)).toBe(true);
+  });
+
+  test("Ship doesn't when hits!= length", () => {
+    expect(ship.isSunk(3, 4)).toBe(false);
   });
 });
 console.log("");
